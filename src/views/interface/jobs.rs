65ea0dc4ef4ba5853f19851b42b5fcad7d0788fd
Eden <necrotechno@riseup.net>
2020-03-02 12:27:31 +0000

    feat(job): convert new job matching to enum

--- src/views/interface/jobs.rs
+++ src/views/interface/jobs.rs
@@ -40,14 +40,15 @@ pub fn update_job_view(siv: &mut Cursive, state_manager: &'static Mutex<StateMan
                     .clone(),
             );
         });
-        let word_finder_job =
-            MalwareHunterView::new(2, move |siv| complete_job(siv, state_manager));
+        let new_job = match state_retr!(state_manager).job.as_ref().unwrap().job_type  {
+            JobType::MalwareHunter => MalwareHunterView::new(2, move |siv| complete_job(siv, state_manager))
+        };
         siv.call_on_name(WORKSPACE_VIEW_NAME, |view: &mut LinearLayout| {
             view.remove_child(1);
             view.insert_child(
                 1,
                 ResizedView::with_full_screen(
-                    Panel::new(word_finder_job)
+                    Panel::new(new_job)
                         .title("Workspace")
                         .title_position(HAlign::Left),
                 ),
