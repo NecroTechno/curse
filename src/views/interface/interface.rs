edd0f03d3edf07851378397f35133b82c2b6fea4
Eden <necrotechno@riseup.net>
2020-03-03 16:47:15 +0000

    refactor: better interface

--- src/views/interface/interface.rs
+++ src/views/interface/interface.rs
@@ -13,8 +13,8 @@ use cursive::align::HAlign;
 use cursive::event::{Event, EventTrigger};
 use cursive::view::{Nameable, View};
 use cursive::views::{
-    Button, Canvas, Dialog, DummyView, EditView, LinearLayout, ListView, OnEventView, PaddedView,
-    Panel, ResizedView, ScrollView, SelectView, TextView,
+    Button, Canvas, Dialog, DummyView, EditView, LinearLayout, ListView, NamedView, OnEventView,
+    PaddedView, Panel, ResizedView, ScrollView, SelectView, TextView,
 };
 use cursive::Cursive;
 
@@ -28,8 +28,71 @@ pub fn interface(siv: &mut Cursive, state_manager: &'static Mutex<StateManager>)
     view_open(siv, state_manager, VIEW_CATEGORY);
 
     let (animator_config, logo) = logo_ani_generator();
+    let menu = generate_menu_view(state_manager);
+    let sidebar = generate_sidebar_view(logo, menu, state_manager);
+    let job_ui = generate_job_ui_view();
 
-    let menu = LinearLayout::horizontal()
+    let layout = PaddedView::lrtb(
+        1,
+        1,
+        1,
+        1,
+        LinearLayout::horizontal().child(job_ui).child(sidebar),
+    );
+
+    siv.add_fullscreen_layer(
+        OnEventView::new(layout)
+            .on_event(Event::Refresh, move |s| {
+                animate(&animator_config, s);
+            })
+            .on_pre_event_inner(EventTrigger::arrows(), move |_s, _e| {
+                focus_se(state_manager)
+            }),
+    );
+}
+
+fn generate_job_ui_view() -> NamedView<LinearLayout> {
+    return LinearLayout::vertical()
+        .child(
+            Panel::new(ResizedView::with_min_height(
+                1,
+                TextView::empty().with_name(JOB_TITLE_VIEW_NAME),
+            ))
+            .title("Job Title")
+            .title_position(HAlign::Left),
+        )
+        .child(ResizedView::with_full_screen(
+            Panel::new(DummyView)
+                .title("Workspace")
+                .title_position(HAlign::Left),
+        ))
+        .with_name(WORKSPACE_VIEW_NAME);
+}
+
+fn generate_sidebar_view(
+    logo: NamedView<TextView>,
+    menu: NamedView<LinearLayout>,
+    state_manager: &'static Mutex<StateManager>,
+) -> LinearLayout {
+    return LinearLayout::vertical()
+        .child(PaddedView::lrtb(2, 2, 1, 1, logo))
+        .child(PaddedView::lrtb(
+            2,
+            2,
+            1,
+            2,
+            TextView::new(format!("Welcome, {}", state_retr!(state_manager).name))
+                .h_align(HAlign::Center),
+        ))
+        .child(Panel::new(PaddedView::lrtb(1, 1, 1, 1, menu)).title("Menu"))
+        .child(ResizedView::with_full_height(
+            // Notifications panel
+            Panel::new(ListView::new().with_name(NOTIFICATION_VIEW_NAME)).title("Notifications"),
+        ));
+}
+
+fn generate_menu_view(state_manager: &'static Mutex<StateManager>) -> NamedView<LinearLayout> {
+    return LinearLayout::horizontal()
         .child(Button::new("Save.", move |_s| {
             state_retr!(state_manager).save();
         }))
@@ -50,57 +113,4 @@ pub fn interface(siv: &mut Cursive, state_manager: &'static Mutex<StateManager>)
         }))
         .child(Button::new("Quit.", |s| s.quit()))
         .with_name(MENU_VIEW_NAME);
-
-    let layout = PaddedView::lrtb(
-        1,
-        1,
-        1,
-        1,
-        LinearLayout::horizontal()
-            .child(
-                LinearLayout::vertical()
-                    .child(
-                        Panel::new(ResizedView::with_min_height(
-                            1,
-                            TextView::empty().with_name(JOB_TITLE_VIEW_NAME),
-                        ))
-                        .title("Job Title")
-                        .title_position(HAlign::Left),
-                    )
-                    .child(ResizedView::with_full_screen(
-                        Panel::new(DummyView)
-                            .title("Workspace")
-                            .title_position(HAlign::Left),
-                    ))
-                    .with_name(WORKSPACE_VIEW_NAME),
-            )
-            .child(
-                LinearLayout::vertical()
-                    .child(PaddedView::lrtb(2, 2, 1, 1, logo))
-                    .child(PaddedView::lrtb(
-                        2,
-                        2,
-                        1,
-                        2,
-                        TextView::new(format!("Welcome, {}", state_retr!(state_manager).name))
-                            .h_align(HAlign::Center),
-                    ))
-                    .child(Panel::new(PaddedView::lrtb(1, 1, 1, 1, menu)).title("Menu"))
-                    .child(ResizedView::with_full_height(
-                        // Notifications panel
-                        Panel::new(ListView::new().with_name(NOTIFICATION_VIEW_NAME))
-                            .title("Notifications"),
-                    )),
-            ),
-    );
-
-    siv.add_fullscreen_layer(
-        OnEventView::new(layout)
-            .on_event(Event::Refresh, move |s| {
-                animate(&animator_config, s);
-            })
-            .on_pre_event_inner(EventTrigger::arrows(), move |_s, _e| {
-                focus_se(state_manager)
-            }),
-    );
 }
